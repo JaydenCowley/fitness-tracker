9c9d3071dabca5d182712fe6856973e007aff46e
Bryson Vatuvei <vatuvei.bryson@yahoo.com>
2022-06-15 20:37:18 -0600

    added more get and post methods

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,16 @@
+const express = require("express");
+
 const getWorkouts = () => 
     fetch('/api/workouts', {
         method: 'GET',
     })
         .then((res) => res.json())
         .then((data) => data);
+
+app.get('/api/workouts', (req, res) => {
+    res.json(`${req.method} request received to get workouts`)
+});
+
+app.post('/api/reviews', (req, res) => {
+    console.info(`${req.method} request recieved to add workout`)
+});
