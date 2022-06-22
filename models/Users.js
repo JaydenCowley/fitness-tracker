818c95d071037424b992ff4b2c2eca8dc71eb9ae
Bryson Vatuvei <vatuvei.bryson@yahoo.com>
2022-06-21 21:02:17 -0600

    added other files that need to be adjusted

--- models/Users.js
+++ models/Users.js
@@ -1,8 +1,12 @@
-const { Model, DataTypes } = require('sequlize'); 
+const { Model, DataTypes } = require('sequelize'); 
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
 
 class User extends Model {
-
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+      }
 }
 
 // create the user table 
