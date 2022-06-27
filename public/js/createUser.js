120362120034448fa07c39da2c57e7b5b95fcf96
Jayden Cowley <jcowley16@gmail.com>
2022-06-27 12:58:25 -0600

    made it so the signup form accually adds users to the database

--- public/js/createUser.js
+++ public/js/createUser.js
@@ -1,8 +1,9 @@
-async function signupFormHandler(event) {
-    event.preventDefault();
+async function signupFormHandler() {
+    
+    // event.preventDefault();
   
     const firstName = document.querySelector('#firstName').value.trim();
-    const lastName = document.querySelector('#lastname').value.trim();
+    const lastName = document.querySelector('#lastName').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
     const confirmPassword = document.querySelector('#confirm-password').value.trim();
@@ -11,19 +12,18 @@ async function signupFormHandler(event) {
       const response = await fetch('/api/users', {
         method: 'post',
         body: JSON.stringify({
-            firstName,
-            lastName,
-            email,
-            password
-        }),
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password
+      }),
         headers: { 'Content-Type': 'application/json' }
       });
-  
       if (response.ok) {
         document.location.replace('/dashboard/');
-      } else {
-        alert(response.statusText);
-      }
-    }
+       } else {
+         alert(response.statusText);
+       }
+     }
   }
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+  document.querySelector('.signup-form').addEventListener('click', signupFormHandler);
