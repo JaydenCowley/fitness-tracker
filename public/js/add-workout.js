8cf307ac5ecbd865e0152828db59967bc720cc9e
clarkspencerc <clarkspencerc@gmail.com>
2022-06-27 23:29:04 -0600

    added add-workout.js file

---
+++ public/js/add-workout.js
@@ -0,0 +1,26 @@
+async function newFormHandler(event) {
+event.preventDefault(); 
+
+    const activity = document.querySelector('input[name="activity"]').value;
+    const duration = document.querySelector('input[name="duration"]').value; 
+    const date = document.querySelector('input[name="date"]').value;  
+
+    const response = await fetch(`/api/user/workouts`, {
+        method: 'POST', 
+        body: JSON.stringify({
+            activity,
+            duration,
+            date
+        }), 
+        headers: {
+            'Content-Type' : "aplication/json" // I don't understand what this is doing
+        }
+    }); 
+    if (response.ok) {
+        document.location.replace('/workoutHistory'); 
+    } else {
+        alert(response.statusText); 
+    }
+}
+
+document.querySelector('.new-workout-form').addEventListener('submit', newFormHandler); 
